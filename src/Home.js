43a56479e9bbceceb324388037991153229bf7de
Xiaomei Du <xiaomeiDu@github.com>
2024-04-17 19:10:58 -0700

    add sidebar

--- src/Home.js
+++ src/Home.js
@@ -7,16 +7,19 @@ function Home() {
   return (
     <div className="home">
       <div className="home__container">
-      <div className="home__sidebar">
-          <Sidebar />
-        </div>
+        <>
+          <div className="home__sidebar">
+            <Sidebar />
+          </div>
 
-        <div className="home__row">
-          <ProductList />
-        </div>
+          <div className="home__row">
+            <ProductList />
+          </div>
+        </>
       </div>
     </div>
   );
 }
+
 
 export default Home;
